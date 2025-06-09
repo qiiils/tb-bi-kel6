ddc380d08ff04bc8e20e8778069938e06e77bf3e
qiiils <001qils@gmail.com>
2025-06-09 18:20:06 +0700

    fillna

--- pages/3_Dropout_Risk_Analyzer.py
+++ pages/3_Dropout_Risk_Analyzer.py
@@ -29,6 +29,8 @@ features = [
 X = df[features]
 y = df['dropout_risk_encoded']
 
+X = X.fillna(X.median())
+
 # Split data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
